39beee57293a21e09bee84a7a56ee925b6a6dadd
Lucas Pereira de Lima <lucaspereiradelima2020@gmail.com>
2024-05-04 16:50:47 -0300

    Lógica para dentro do limite e fora

--- js/app.js
+++ js/app.js
@@ -4,18 +4,21 @@ function verificaVelocidade() {
     var velocidade = document.getElementById("velocidade").value;
     
     var main = document.querySelector("main");
-    console.log(main);
     var p = document.createElement("p");
+    var img = document.createElement("img");
     main.appendChild(p);
+    console.log(img);
     
     if (velocidade > 107.00) {
         var paragrafoConteudo = document.createTextNode("Você ultrapassou o limite de velocidade, portanto foi multado!");
+        img.src = "../img/multado.jpg";
+    } else {
+        var paragrafoConteudo = document.createTextNode("Velocidade está dentro do limite, parabéns!");
+        img.src = "../img/sem-multa.jpg";
     }
 
     p.appendChild(paragrafoConteudo);
-    //console.log(p);
-    //main.appendChild(paragrafoConteudo);
+    main.appendChild(img);
 }
 
 botao.addEventListener("click", verificaVelocidade);
-console.log(botao);
