9c462fea33d08671ea63a11bbb9efe413eb2eed7
Lucas Pereira de Lima <lucaspereiradelima2020@gmail.com>
2024-05-05 10:39:52 -0300

    Posicionando os elementos

--- js/app.js
+++ js/app.js
@@ -1,15 +1,15 @@
 let botao = document.getElementById("botao");
+let p = document.createElement("p");
+let main = document.querySelector("main");
+let img = document.createElement("img");
 
 function verificaVelocidade() {
     var velocidade = document.getElementById("velocidade").value;
-    var p = document.createElement("p");
-    var main = document.querySelector("main");
-    var img = document.createElement("img");
-
     if (velocidade === "") {
         var paragrafoConteudo = document.createTextNode("Adicione o valor da velocidade!");
         p.style.color = "red";
         p.style.fontSize = "20px";
+        img.src = "";
     } else {
         if (velocidade > 107.00) {
             var paragrafoConteudo = document.createTextNode("Você ultrapassou o limite de velocidade, portanto foi multado!");
@@ -17,13 +17,23 @@ function verificaVelocidade() {
         } else {
             var paragrafoConteudo = document.createTextNode("Velocidade está dentro do limite, parabéns!");
             img.src = "../img/sem-multa.jpg";
+            p.removeAttribute("style");
         }
-        botao.disabled = true;
     }
 
+    botao.disabled = true;
     main.appendChild(p);
     p.appendChild(paragrafoConteudo);
     main.appendChild(img);
+
+    setTimeout(apagaRegistros, 3000);
+}
+
+function apagaRegistros() {
+    p.textContent = "";
+    main.removeChild(p);
+    main.removeChild(img);
+    botao.disabled = false;
 }
 
 botao.addEventListener("click", verificaVelocidade);
