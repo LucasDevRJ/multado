9999e522b10f1d74aea92483ee31503ed2cade90
Lucas Pereira de Lima <lucaspereiradelima2020@gmail.com>
2024-05-05 10:02:19 -0300

    Alternativas para várias condições

--- js/app.js
+++ js/app.js
@@ -2,21 +2,26 @@ let botao = document.getElementById("botao");
 
 function verificaVelocidade() {
     var velocidade = document.getElementById("velocidade").value;
-    
-    var main = document.querySelector("main");
     var p = document.createElement("p");
+    var main = document.querySelector("main");
     var img = document.createElement("img");
-    main.appendChild(p);
-    console.log(img);
-    
-    if (velocidade > 107.00) {
-        var paragrafoConteudo = document.createTextNode("Você ultrapassou o limite de velocidade, portanto foi multado!");
-        img.src = "../img/multado.jpg";
+
+    if (velocidade === "") {
+        var paragrafoConteudo = document.createTextNode("Adicione o valor da velocidade!");
+        p.style.color = "red";
+        p.style.fontSize = "20px";
     } else {
-        var paragrafoConteudo = document.createTextNode("Velocidade está dentro do limite, parabéns!");
-        img.src = "../img/sem-multa.jpg";
+        if (velocidade > 107.00) {
+            var paragrafoConteudo = document.createTextNode("Você ultrapassou o limite de velocidade, portanto foi multado!");
+            img.src = "../img/multado.jpg";
+        } else {
+            var paragrafoConteudo = document.createTextNode("Velocidade está dentro do limite, parabéns!");
+            img.src = "../img/sem-multa.jpg";
+        }
+        botao.disabled = true;
     }
 
+    main.appendChild(p);
     p.appendChild(paragrafoConteudo);
     main.appendChild(img);
 }
